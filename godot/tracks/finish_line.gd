603f17a43fea425e2d7c51ffd52fa70ae62e703c
benochansong <bethbes6126@gmail.com>
2026-08-10 03:28:53 +0900

    feat: require checkpoints before Grand Prix finish

--- godot/tracks/finish_line.gd
+++ godot/tracks/finish_line.gd
@@ -5,4 +5,11 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is WildDashCharacterController:
-		RaceManager.record_finish(body)
+		if RaceManager.can_finish(body):
+			RaceManager.record_finish(body)
+		else:
+			print("FINISH VALIDATION BLOCK racer=%s checkpoints=%d/%d" % [
+				RaceManager.get_racer_label(body),
+				RaceManager.get_checkpoint_progress(body),
+				RaceManager.get_checkpoint_count(),
+			])
