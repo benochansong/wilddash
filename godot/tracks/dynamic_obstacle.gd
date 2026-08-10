edd8e445c3edce4bc98f743ed03524f0fa42d5a2
benochansong <bethbes6126@gmail.com>
2026-08-10 12:46:29 +0900

    feat: scale moving hazards by selected difficulty

--- godot/tracks/dynamic_obstacle.gd
+++ godot/tracks/dynamic_obstacle.gd
@@ -12,14 +12,26 @@ enum MotionType {
 
 var _origin := Vector3.ZERO
 var _elapsed := 0.0
+var _difficulty_scale := 1.0
 
 func _ready() -> void:
 	_origin = position
+	var profile := WildDashDifficultySystem.get_profile(GameManager.difficulty)
+	_difficulty_scale = float(profile.get("obstacle_speed_scale", 1.0))
+	print("OBSTACLE DIFFICULTY name=%s mode=%s speed_scale=%.2f" % [
+		name,
+		WildDashDifficultySystem.get_display_name(GameManager.difficulty),
+		_difficulty_scale,
+	])
 
 func _physics_process(delta: float) -> void:
 	_elapsed += delta
+	var effective_speed := motion_speed * _difficulty_scale
 	match motion_type:
 		MotionType.ROTATE:
-			rotation.y += motion_speed * delta
+			rotation.y += effective_speed * delta
 		MotionType.SWEEP:
-			position = _origin + global_transform.basis.x.normalized() * sin(_elapsed * motion_speed) * amplitude
+			position = _origin + global_transform.basis.x.normalized() * sin(_elapsed * effective_speed) * amplitude
+
+func get_effective_motion_speed() -> float:
+	return motion_speed * _difficulty_scale
