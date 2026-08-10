7e4262490f92400417db7901d1ab37bd502bba45
benochansong <bethbes6126@gmail.com>
2026-08-10 12:31:30 +0900

    gameplay: make dynamic obstacle speed difficulty-aware

--- godot/tracks/dynamic_obstacle.gd
+++ godot/tracks/dynamic_obstacle.gd
@@ -12,9 +12,19 @@ enum MotionType {
 
 var _origin := Vector3.ZERO
 var _elapsed := 0.0
+var _base_motion_speed := 1.6
 
 func _ready() -> void:
 	_origin = position
+	_base_motion_speed = motion_speed
+
+func set_difficulty_speed_scale(scale: float) -> void:
+	if _base_motion_speed <= 0.0:
+		_base_motion_speed = motion_speed
+	motion_speed = _base_motion_speed * clampf(scale, 0.5, 1.6)
+
+func get_base_motion_speed() -> float:
+	return _base_motion_speed
 
 func _physics_process(delta: float) -> void:
 	_elapsed += delta
